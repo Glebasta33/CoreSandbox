a6c710568e420693ccf3efcde5ca68a89d058803
Gleb Trusov <gleb.trusow@yandex.ru>
2024-01-19 15:29:20 +0300

    6.2. Примитивные и другие базовые типы.

---
+++ src/main/java/kotlin_in_action/types/Main.java
@@ -0,0 +1,10 @@
+package kotlin_in_action.types;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 0;
+        // List<int> numbers <- Type argument cannot be of primitive type
+    }
+}
