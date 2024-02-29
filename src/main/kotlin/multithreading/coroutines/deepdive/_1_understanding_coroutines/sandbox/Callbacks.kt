e91b2e0b2eedf707e85295aad3e7f8d171929bd9
Gleb Trusov <gleb.trusow@yandex.ru>
2024-02-29 08:35:24 +0300

    Заметки по коллбекам

--- src/main/kotlin/multithreading/coroutines/deepdive/_1_understanding_coroutines/sandbox/Callbacks.kt
+++ src/main/kotlin/multithreading/coroutines/deepdive/_1_understanding_coroutines/sandbox/Callbacks.kt
@@ -2,17 +2,34 @@ package multithreading.coroutines.deepdive._1_understanding_coroutines.sandbox
 
 import kotlin.concurrent.thread
 
+/**
+ * Коллбек - это по сути объект анонимного класса.
+ * В main - находится реализация коллбеков, которые передаются в методы loadUserInfo и loadDataFromServer.
+ * Внутри методов loadUserInfo и loadDataFromServer происходит вызов этих коллбеков (объектов анонимного класса), которые живут в main, из фоновых потоков.
+ */
 fun main() {
     println("Before ${Thread.currentThread().name}")
-    loadDataFromServer { data ->
-        println(data)
+    loadUserInfo(id = "123") { userInfo: String ->
+        loadDataFromServer(userInfo = userInfo) { data ->
+            println(data)
+        }
     }
     println("After ${Thread.currentThread().name}")
 }
 
-fun loadDataFromServer(callback: (String) -> Unit) {
+fun loadUserInfo(id: String, callback: (userInfo: String) -> Unit) {
     thread {
         Thread.sleep(2000)
-        callback.invoke("Data ${Thread.currentThread().name}")
+        callback.invoke("user-$id")
+        println("loadUserInfo finished in ${Thread.currentThread().name}")
     }
 }
+
+fun loadDataFromServer(userInfo: String, callback: (String) -> Unit) {
+    thread {
+        Thread.sleep(2000)
+        callback.invoke("Data for $userInfo")
+        println("loadDataFromServer finished in ${Thread.currentThread().name}")
+    }
+}
+
