4e21ff170c81f0e2378ce3fd80dff0ca2df690d7
Gleb Trusov <gletrusov@sberbank.ru>
2024-03-25 18:07:43 +0300

    Кейс с ковариантностью

--- src/main/kotlin/kotlin_lang/kotlin_in_action/_9_generics/sandbox/Variance.kt
+++ src/main/kotlin/kotlin_lang/kotlin_in_action/_9_generics/sandbox/Variance.kt
@@ -1,7 +1,44 @@
 package kotlin_lang.kotlin_in_action._9_generics.sandbox
 
-//TODO: Прощупать кейсы с вариантностью
+private interface Loot {
+    fun fetchWeight(): Float
+}
+
+private class Weapon(
+    private val name: String,
+    val weight: Float,
+    private var sharpness: Int
+) : Loot {
+    override fun fetchWeight(): Float = weight
+    fun sharpen() { sharpness.inc() }
+}
+
+private class Box<out T : Loot>(initialLoot: List<T>) {
+    private val loot = initialLoot.toMutableList()
+    val size: Int
+        get() = loot.size
+    fun getLootByIndex(index: Int) = loot[index]
+}
 
 fun main() {
+    val weaponBox = Box<Weapon>(
+        initialLoot = listOf(
+            Weapon("knife", 1.7f, 80),
+            Weapon("sword", 5.2f, 95)
+        )
+    )
 
+    /**
+     * Функция принимает тип Box<Loot>, но я передаю Box<Weapon>.
+     * Чтобы это было возможно, необходимо добавить out - ковариантность.
+     */
+    fun calculateWeightOfBox(loot: Box<Loot>) {
+        var fullWeight: Float = 0f
+        for (w in 0 until loot.size ) {
+            fullWeight += loot.getLootByIndex(w).fetchWeight()
+        }
+        println("fullWeight: $fullWeight")
+    }
+
+    calculateWeightOfBox(weaponBox)
 }
