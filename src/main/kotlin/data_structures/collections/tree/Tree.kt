4e64bd594491878bdab2c7134904edc196b829b8
Gleb Trusov <gleb.trusow@yandex.ru>
2024-03-30 09:54:39 +0300

    Добавил TODO по Tree и sealed class

---
+++ src/main/kotlin/data_structures/collections/tree/Tree.kt
@@ -0,0 +1,10 @@
+package data_structures.collections.tree
+
+import java.util.TreeSet
+
+//TODO: https://www.baeldung.com/kotlin/binary-tree
+
+fun main() {
+    //TODO: https://proglang.su/java/treeset-class
+    val tree: TreeSet<Int> = TreeSet()
+}
---
+++ src/main/kotlin/kotlin_lang/kotlin_in_action/_4_class_interface/sealed_class/SealedClass.kt
@@ -0,0 +1,3 @@
+package kotlin_lang.kotlin_in_action._4_class_interface.sealed_class
+
+//TODO: Декомпилировать sealed class и попробовать реализовать на Java + см. пример в https://moshenskyi.medium.com/kotlin-under-the-hood-the-magic-of-classes-part-2-f0f1aea13056
