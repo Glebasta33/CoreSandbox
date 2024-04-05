8c9c8286ad2d2b32ee6fe440fa1124a3ba3d3c74
Gleb Trusov <gletrusov@sberbank.ru>
2024-04-05 11:00:31 +0300

    Пощупал treeset

--- src/main/kotlin/data_structures/collections/tree/Tree.kt
+++ src/main/kotlin/data_structures/collections/tree/Tree.kt
@@ -1,10 +1,34 @@
 package data_structures.collections.tree
 
+import java.util.SortedSet
 import java.util.TreeSet
 
 //TODO: https://www.baeldung.com/kotlin/binary-tree
 
 fun main() {
     //TODO: https://proglang.su/java/treeset-class
-    val tree: TreeSet<Int> = TreeSet()
+
+    // https://www.baeldung.com/java-tree-set:
+
+    val treeSet: SortedSet<String> = TreeSet()
+
+    treeSet.add("bbc")
+    treeSet.add("b")
+    treeSet.add("abc")
+    treeSet.add("aa")
+
+    treeSet.forEach { string ->
+        print("$string ") // aa abc b bbc
+    }
+
+    val treeSetSortedByLength: SortedSet<String> = TreeSet(Comparator.comparing(String::length))
+
+    treeSetSortedByLength.add("bbc")
+    treeSetSortedByLength.add("b")
+    treeSetSortedByLength.add("abc") // returns false
+    treeSetSortedByLength.add("aa")
+
+    treeSetSortedByLength.forEach { string ->
+        print("$string ") // b aa bbc
+    }
 }
