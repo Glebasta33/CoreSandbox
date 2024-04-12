acb2d7ea4477e3df9bdd7a573aa29226fc066bcc
Gleb Trusov <gletrusov@sberbank.ru>
2024-04-12 16:59:19 +0300

    Начало изучения паттернов проектирования

---
+++ src/main/kotlin/design_patterns/DesignPatterns.md
@@ -0,0 +1,7 @@
+
+## [Software Design Patterns Tutorial](https://www.geeksforgeeks.org/software-design-patterns/?ref=lbp)
+
+### Types of Software Design Patterns
+1. Creational
+2. Structural
+3. Behavioral
---
+++ src/main/kotlin/design_patterns/creational/factory_method/FactoryMethod.kt
@@ -0,0 +1,6 @@
+package design_patterns.creational.factory_method
+
+// TODO: Реализовать Java пример фабричного метода на Kotlin https://www.geeksforgeeks.org/factory-method-for-designing-pattern/?ref=lbp
+fun main() {
+
+}
