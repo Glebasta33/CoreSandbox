54d405e59fa2709ab54452375d2aa3b79c3361e9
Gleb Trusov <gletrusov@sberbank.ru>
2024-10-14 18:29:48 +0300

    Эксперименты над корутинами

---
+++ src/main/kotlin/multithreading/coroutines/androidbroadcast/_1_coroutine/CoroutineMainSandbox1.kt
@@ -0,0 +1,22 @@
+package multithreading.coroutines.androidbroadcast._1_coroutine
+
+import kotlinx.coroutines.*
+import kotlin.random.Random
+
+fun main(): Unit = runBlocking {
+    val deferred = async {
+        val name = "Param for suspend function"
+        val calculationResult = withContext(Dispatchers.Default) {
+            calculation(name)
+        }
+        calculationResult
+    }
+
+    val deferredResult = deferred.await()
+    println(deferredResult)
+}
+
+suspend fun calculation(name: String): String {
+    delay(Random.nextLong(5000))
+    return "Done on ${Thread.currentThread().name}. $name"
+}
