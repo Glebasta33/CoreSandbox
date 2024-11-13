ad6698a45fcd07d75afbe3885f12e638c6309291
Gleb Trusov <gleb.trusow@yandex.ru>
2024-11-13 14:52:14 +0300

    Заметки по Flow builders/converter (Coroutines: Deep Dive).

---
+++ src/main/kotlin/coroutines/coroutines_deepdive/_3_channel_and_flow/builders/Converters.kt
@@ -0,0 +1,28 @@
+package coroutines.coroutines_deepdive._3_channel_and_flow.builders
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.asFlow
+
+suspend fun main() {
+    listOf(1, 2, 3, 4, 5)
+        // or setOf(...), or sequenceOf(...)
+        .asFlow()
+        .collect { print(it) }
+
+    val function: suspend () -> String = suspend {
+        delay(1000)
+        "UserName"
+    }
+
+    function.asFlow()
+        .collect{ println(it) }
+
+    ::getUserName
+        .asFlow()
+        .collect{ println(it) }
+}
+
+suspend fun getUserName(): String {
+    delay(1000)
+    return "UserName"
+}
